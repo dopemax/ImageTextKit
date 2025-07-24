6588219a8a7cf0591e71cb4b81f7e42966b13ae8
dopemax <1003990034@qq.com>
2025-07-24 14:31:30 +0800

    Initial Commit

---
+++ ImageTextKit/ImageTextKit.h
@@ -0,0 +1,18 @@
+//
+//  ImageTextKit.h
+//  ImageTextKit
+//
+//  Created by keji001 on 2025/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageTextKit.
+FOUNDATION_EXPORT double ImageTextKitVersionNumber;
+
+//! Project version string for ImageTextKit.
+FOUNDATION_EXPORT const unsigned char ImageTextKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageTextKit/PublicHeader.h>
+
+
